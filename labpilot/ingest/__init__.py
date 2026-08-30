b58b4e8c36b8ec7f43db19309b05e202333cb812
a1mohamad <a1.mohamad.askari@gmail.com>
2026-08-30 16:06:06 +0330

    refactor: update imports in __init__.py to include chunk_bytes and LoaderError

--- labpilot/ingest/__init__.py
+++ labpilot/ingest/__init__.py
@@ -1,10 +1,13 @@
-from labpilot.ingest.chunker import chunk_file, chunk_text
+from labpilot.ingest.chunker import chunk_bytes, chunk_file
 from labpilot.ingest.contracts import Chunk, Piece, Side
+from labpilot.ingest.errors import LoaderError, NotUtf8Text
 
 __all__ = [
     "Chunk",
+    "LoaderError",
+    "NotUtf8Text",
     "Piece",
     "Side",
     "chunk_file",
-    "chunk_text",
+    "chunk_bytes",
 ]
