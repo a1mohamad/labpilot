54dce4e30de2f9ee552dbaf2cef314f549894b75
a1mohamad <a1.mohamad.askari@gmail.com>
2026-09-08 00:37:10 +0330

    feat: update retrieval module exports to include RRF components

--- labpilot/retrieval/__init__.py
+++ labpilot/retrieval/__init__.py
@@ -1,6 +1,10 @@
 from labpilot.retrieval.dumb import INPUT_BUDGET, select
+from labpilot.retrieval.fusion import RRF_K, RRF_WEIGHTS, weighted_rrf
 
 __all__ = [
-    "select",
     "INPUT_BUDGET",
+    "RRF_K",
+    "RRF_WEIGHTS",
+    "select",
+    "weighted_rrf",
 ]
